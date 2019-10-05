e653d45297279cdf6353c4b296cec9d08732a3ed
Allen Muncy <allen.w.muncy@gmail.com>
2019-10-05 01:17:47 -0400

    Don't require chrome ext

--- app/resources/assets/js/store/store.js
+++ app/resources/assets/js/store/store.js
@@ -2,16 +2,23 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/masterReducer";
 import thunk from 'redux-thunk';
 import { dispatchChannel, hydrate } from "./connections/resources/applicationActions";
+var middleware;
+
+if(window.hasOwnProperty("__REDUX_DEVTOOLS_EXTENSION__")) {
+	middleware = compose(
+		applyMiddleware(thunk),
+		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	);
+} else {
+	middleware = applyMiddleware(thunk);
+}
 
 const store = () => {
 
 	const newStore = createStore(rootReducer, {
 			habits: []
 		}, 
-		compose(
-			applyMiddleware(thunk),
-			window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-		)
+		middleware
 	);
 
 	openChannels(newStore);
