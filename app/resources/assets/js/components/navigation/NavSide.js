1846164041b1933ca7fa3a8a4c6b96591e341af5
Allen Muncy <allen.w.muncy@gmail.com>
2019-10-14 20:46:06 -0400

    Update version

--- app/resources/assets/js/components/navigation/NavSide.js
+++ app/resources/assets/js/components/navigation/NavSide.js
@@ -108,7 +108,7 @@ class AppNav extends Component {
                 </ul>
                 <footer className="underview">
                     <div className="version">
-                        Habit Builder pre-alpha release 0.0.57
+                        Habit Builder pre-alpha release 0.0.59
                     </div>
 	                <div className="logout-section">
     	                <a href="/logout"
