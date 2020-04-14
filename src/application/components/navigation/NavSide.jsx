1cfe13c4160481a4edc51124f942e705afad8940
Allen Muncy <allen.w.muncy@gmail.com>
2020-04-13 22:24:23 -0400

    Bump version

--- src/application/components/navigation/NavSide.jsx
+++ src/application/components/navigation/NavSide.jsx
@@ -124,7 +124,7 @@ class AppNav extends Component {
                 </ul>
                 <footer className="underview">
                     <div className="version">
-                        Habit Builder pre-alpha release 0.0.71
+                        Habit Builder pre-alpha release 0.0.72
                     </div>
 	                <div className="logout-section">
     	                <a href="/logout"
