be2ca065d9906fe14d3f60b2d22f426d8272b899
Allen Muncy <allen.w.muncy@gmail.com>
2020-09-05 18:50:15 -0400

    Improve layout

--- src/application/pages/FeedbackPage.jsx
+++ src/application/pages/FeedbackPage.jsx
@@ -13,13 +13,17 @@ class Feedback extends Component {
 		return (
 			<>
 				<HeaderDefault />
-				<div className="feedback">
-					<p>Thanks in advance, feedback is great</p>
-					<form action="/feedback" method="POST">
-						<textarea ref={input=>this.nameInput=input} name="feedback" placeholder="Your feedback..."></textarea>
-						<input type="hidden" name="userToken" value={localStorage.getItem("id")} />
-						<button className="btn btn-primary" type="submit">Submit</button>
-					</form>
+				<div className="home-layout">
+      				<div className="home-main">
+						<div className="feedback">
+							<p>Thanks in advance, feedback is great</p>
+							<form action="/feedback" method="POST">
+								<textarea ref={input=>this.nameInput=input} name="feedback" placeholder="Your feedback..."></textarea>
+								<input type="hidden" name="userToken" value={localStorage.getItem("id")} />
+								<button className="btn btn-primary" type="submit">Submit</button>
+							</form>
+						</div>
+					</div>
 				</div>
 			</>
 		);
